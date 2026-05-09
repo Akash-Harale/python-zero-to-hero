d43a812cf78ea216cf085179902e70dd27a3c4b4
Akash-Harale <akash.v.harale@gmail.com>
2026-05-09 12:43:15 +0530

    variables and Types

---
+++ Variables and Types/index.py
@@ -0,0 +1,12 @@
+# JavaScript
+
+# let name = "Akash"
+# const age = 25
+# let isActive = true
+# let nothing = null
+
+# Python — no let/const/var, no semicolons
+name = "Akash"
+age = 25
+is_active = True      # capital T/F
+nothing = None        # not null
