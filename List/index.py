16082eec3334043d54efc090e9883f17ad86f142
Akash-Harale <akash.v.harale@gmail.com>
2026-05-09 12:44:46 +0530

    List

---
+++ List/index.py
@@ -0,0 +1,20 @@
+# JS
+
+# let arr = [1, 2, 3, 4, 5]
+# arr.push(6)
+# arr.pop()
+# arr.length
+# arr.map(x => x * 2)
+# arr.filter(x => x > 2)
+# arr.includes(3)
+# arr.slice(1, 3)
+
+# Python
+arr = [1, 2, 3, 4, 5]
+arr.append(6)          # push = append
+arr.pop()              # same
+len(arr)               # .length = len()
+[x * 2 for x in arr]  # map = list comprehension
+[x for x in arr if x > 2]   # filter
+3 in arr               # includes = in
+arr[1:3]               # slice = slicing
