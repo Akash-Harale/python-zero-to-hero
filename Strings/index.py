d996babc65ea68b8c449c86351b3012f36219ce7
Akash-Harale <akash.v.harale@gmail.com>
2026-05-09 12:44:23 +0530

    Strings

---
+++ Strings/index.py
@@ -0,0 +1,14 @@
+#  JS
+
+# let msg = `Hello ${name}, you are ${age}`
+# "hello".toUpperCase()
+# "hello world".split(" ")
+# "  hi  ".trim()
+
+# Python
+name="Akash"
+age=27
+msg = f"Hello {name}, you are {age}"   # f-string = template literal
+"hello".upper()
+"hello world".split(" ")
+"  hi  ".strip()                        # trim = strip
